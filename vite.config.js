b979f41ce74920b476e3ce5252e8422b734e2b00
Fyne Tonye Angala <angalafynetonye@gmail.com>
2025-07-16 23:58:58 +0100

    updated vite config

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,15 @@
+
+// // vite.config.js
 // import { defineConfig } from 'vite'
 // import react from '@vitejs/plugin-react'
-// import tailwindcss from '@tailwindcss/vite'
 
-// // https://vite.dev/config/
 // export default defineConfig({
-//   plugins: [
-//     react(),
-//     tailwindcss(),
-//   ],
+//   plugins: [react()],
+//   css: {
+//     postcss: './postcss.config.js',
+//   },
 // })
+
 
 // vite.config.js
 import { defineConfig } from 'vite'
@@ -16,7 +17,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  css: {
-    postcss: './postcss.config.js',
-  },
+  base: './', 
 })
