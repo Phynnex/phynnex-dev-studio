19d31437ee555a9f06e8cfecd8d321860c9773de
Fyne Tonye Angala <66461071+Phynnex@users.noreply.github.com>
2025-08-12 16:33:43 +0100

    refactor: load fonts with next/font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,25 @@
 import type { AppProps } from 'next/app';
 import '../styles/globals.css';
 import Layout from '../components/Layout';
+import { Inter, Montserrat } from 'next/font/google';
+
+const inter = Inter({
+  subsets: ['latin'],
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+  variable: '--font-inter',
+});
+const montserrat = Montserrat({
+  subsets: ['latin'],
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+  variable: '--font-montserrat',
+});
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <div className={`${inter.variable} ${montserrat.variable} font-inter`}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </div>
   );
 }
