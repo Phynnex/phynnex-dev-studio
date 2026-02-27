b38263497bfcc169890bdd945cd7e12145422e68
Fyne Tonye Angala <angalafynetonye@gmail.com>
2026-02-27 21:57:26 +0100

    fix: update flat eslint config and lint scripts

--- eslint.config.js
+++ eslint.config.js
@@ -3,12 +3,15 @@
 const base = require('neostandard')({})
 
 module.exports = [
+  {
+    ignores: ['next-env.d.ts', '.next/**', 'node_modules/**']
+  },
   ...base,
   {
     name: 'old-standard',
     rules: {
       'no-var': 'off',
-      'object-shorthand': 'off',
+      'object-shorthand': 'off'
     }
   }
 ]
