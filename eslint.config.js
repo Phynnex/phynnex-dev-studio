07b86dbb87dca2ef934c57895fe990d07408721c
Fyne Tonye Angala <angalafynetonye@gmail.com>
2026-02-27 22:08:27 +0100

    fix: use valid flat eslint config for next/react

--- eslint.config.js
+++ eslint.config.js
@@ -1,16 +1,30 @@
 'use strict'
 
-const next = require('eslint-config-next')
+const nextPlugin = require('@next/eslint-plugin-next')
+const reactPlugin = require('eslint-plugin-react')
 
 module.exports = [
   {
     ignores: ['next-env.d.ts', '.next/**', 'node_modules/**']
   },
-  ...next,
   {
+    files: ['**/*.{js,jsx,ts,tsx}'],
+    plugins: {
+      '@next/next': nextPlugin,
+      react: reactPlugin
+    },
     rules: {
+      ...nextPlugin.configs.recommended.rules,
+      ...nextPlugin.configs['core-web-vitals'].rules,
+      ...reactPlugin.configs.recommended.rules,
+      'react/react-in-jsx-scope': 'off',
       'no-var': 'off',
       'object-shorthand': 'off'
+    },
+    settings: {
+      react: {
+        version: 'detect'
+      }
     }
   }
 ]
